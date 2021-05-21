441a8c881391c0219179c9d4397570576b422a1c
Natalia Cabrera <n.alvescabrera@gmail.com>
2021-05-21 17:46:38 -0300

    Adicionnando mais práticas com relacionamentos

--- gerenciador-de-cursos/src/br/com/alura/TestaListaDeAula.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaListaDeAula.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 
 
@@ -8,7 +9,7 @@ public class TestaListaDeAula {
 	
 	public static void main(String[] args) {
 		Aula a1 = new Aula("Revisitando as ArrayLists",21);
-		Aula a2 = new Aula("Lista de Objetos",15);
+		Aula a2 = new Aula("Lista de Objetos",20);
 		Aula a3 = new Aula("Relacionamento de listas e objetos",15);
 		
 		ArrayList<Aula> aulas = new ArrayList<>();
@@ -21,6 +22,12 @@ public class TestaListaDeAula {
 		Collections.sort(aulas);
 		
 		System.out.println(aulas);
+		
+		Collections.sort(aulas,Comparator.comparing(Aula::getTempo));
+		
+		System.out.println(aulas);
+		
+		aulas.sort(Comparator.comparing(Aula::getTempo));
 	}
 
 }
